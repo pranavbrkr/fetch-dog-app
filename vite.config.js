aeabb6e2851c085f9a063c71f8d10decbd21824f
pranavbrkr <pranavbrkr@gmail.com>
2025-02-07 15:44:08 -0700

    repo name fix

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/fetch-dog-app/',
+  base: '/fetch-dogs-app/',
   plugins: [
     vue(),
     vueDevTools(),
