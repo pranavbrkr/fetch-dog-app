65b9ff4333975d703d71076af9892c66773d1cea
pranavbrkr <pranavbrkr@gmail.com>
2025-02-07 15:05:17 -0700

    base repo correction

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/fetch-dog-apps/',
+  base: '/fetch-dog-app/',
   plugins: [
     vue(),
     vueDevTools(),
